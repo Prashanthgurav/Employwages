85d2e292d891a98d81aa9bb29506906f04a93584
prashantgurav <guravp@gmail.com>
2023-04-10 03:11:25 +0530

    [Prashant] employe wages with Switch case

---
+++ EmployewagesUsngswitch/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+
+class Switchcase
+{
+    static void Main(string[] args)
+    {
+        const int isFullTime = 1;
+        const int isPartTime = 2;
+        int EMP_RATE_PER1_HOUR = 20;
+
+        int emphrs = 0;
+        int empwage = 0;
+        Console.WriteLine("Welcome to Employee Wage Computation Problem");
+        Random random = new Random();
+
+        int empAttendance = random.Next(0, 3);
+        switch (empAttendance)
+        {
+            case isFullTime:
+                emphrs = 8;
+                break;
+
+            case isPartTime:
+                emphrs = 8;
+                break;
+
+            default:
+                emphrs = 0;
+                break;
+        }
+
+
+        empwage = emphrs * EMP_RATE_PER1_HOUR;
+        Console.WriteLine("Emp Wage   " + empwage);
+
+    }
+}
