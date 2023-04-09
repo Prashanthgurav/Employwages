aedca93c82e8a095a99d13af28ea55ad59fae7a4
prashantgurav <guravp@gmail.com>
2023-04-10 03:33:30 +0530

    [Prashant] Checking Presnt Ubsent

---
+++ EmployPresentUpsent/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int IS_FULL_TIME = 1;
+        Console.WriteLine("Welcome to Employee Wage Computation Problem");
+        Random random = new Random();
+
+        int empCheck = random.Next(0, 2);
+        if (empCheck == IS_FULL_TIME)
+        {
+            Console.WriteLine("Employ is present");
+        }
+        else
+        {
+            Console.WriteLine("Employ is absent");
+        }
+    }
+}
